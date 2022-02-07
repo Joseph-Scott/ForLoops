1716abd9e42f8e6bb3ecad139ee91e6345f0dd3a
Joe Scott <jbs198585@gmail.com>
2022-02-07 06:47:01 -0500

    More for loop examples

--- app.js
+++ app.js
@@ -17,6 +17,10 @@
 //   console.log(i);
 // }
 
-for (i = 100; i >= 0; i -= 10) {
+// for (i = 100; i >= 0; i -= 10) {
+//   console.log(i);
+// }
+
+for (let i = 10; i <= 1000; i *= 10) {
   console.log(i);
 }
