0094e07c43eea65bda9f162a486509bdd264059f
Joe Scott <jbs198585@gmail.com>
2022-02-08 06:25:24 -0500

    Nested loops

--- app.js
+++ app.js
@@ -345,6 +345,33 @@ const animals = [
 //   console.log(i, animals[i]);
 // }
 
-for (let i = animals.length - 1; i >= 0; i--) {
-  console.log(animals[i]);
+// for (let i = animals.length - 1; i >= 0; i--) {
+//   console.log(animals[i]);
+// }
+
+// const seatingChart = [
+//   ["Kristen", "Erik", "Namita"],
+//   ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+//   ["Yuma", "Sakura", "Jack", "Erika"],
+// ];
+
+// for (let i = 1; i < 10; i++) {
+//   console.log(`i is: ${i}`);
+//   for (let j = 1; j < 4; j++) {
+//     console.log(`      j is: ${j}`);
+//   }
+// }
+
+const seatingChart = [
+  ["Kristen", "Erik", "Namita"],
+  ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+  ["Yuma", "Sakura", "Jack", "Erika"],
+];
+
+for (let i = 0; i < seatingChart.length; i++) {
+  const row = seatingChart[i];
+  console.log(`ROW #${i + 1}`);
+  for (let j = 0; j < row.length; j++) {
+    console.log(row[j]);
+  }
 }
